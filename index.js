0b93d5de7e1a1e98be0d584467938180dd02fa7f
[elijah_mbaabu] <[elijahmbaabu1@gmail.com]>
2021-03-17 04:11:35 -0800

    updating and deleting data

--- index.js
+++ index.js
@@ -46,6 +46,8 @@ app.get('/routines/:id',function (req,res) {
     });
     if(!routine){
         return res.status(404).send("the routine not found. thanks");
+    } else{
+        res.status(200).send("Request successful")
     }
     console.log("routine selected", routine);
     return res.send(routine);
@@ -54,9 +56,40 @@ app.post( '/routines', function (req,res) {
    console.log("request has a body", req.body);
    let newRoutine = req.body;
    routines.push(newRoutine);
-    return res.send("well done!");
+    return res.status(200).send("item added successfully");
 
 })
+
+app.put('/routines/:id',function (req,res) {
+
+    let found = routines.find(function (item){
+        return item.id === parseInt(req.params.id);
+    })
+    if (found){
+        let updated = {
+            id: found.id,
+            activity: req.body.activity
+        };
+        let targetRoutine = routines.indexOf(found);
+        routines.splice(targetRoutine,1,updated);
+        res.sendStatus(204);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.delete('/routines/:id', function (req,res){
+    let found = routines.find(function (item){
+        return item.id === parseInt(req.params.id);
+    });
+    if(found){
+        let targetRoutine = routines.indexOf(found);
+        routines.splice(targetRoutine,1);
+        res.sendStatus(204)
+    } else{
+        res.sendStatus(404)
+    }
+});
 
 
 app.listen(8000,function () {
